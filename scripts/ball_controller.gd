d23032c186e0f869041b5def903feb29ffe36d93
Maiko Tuitupou Jr <tuitupoumaiko@gmail.com>
2021-09-03 11:31:48 -0600

    [ADD] Fixed ball eratic behavior at start and clamped bouncing to prevent vertical bouncing

--- scripts/ball_controller.gd
+++ scripts/ball_controller.gd
@@ -4,14 +4,20 @@ extends KinematicBody2D
 
 
 # Public variables
-export var speed:float = 100
-var direction:Vector2
+export var speed: float = 100
+export var vertical_clamp: float = 0.75
+var direction: Vector2
+
+# Private variables
+var _has_bounced_once: bool
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	speed /= 2  # Halve the speed until the first bounce
+	_has_bounced_once = false
 	randomize()
-	direction = Vector2(rand_range(-1,1), rand_range(-0.5,0.5)).normalized()
+	direction = Vector2(rand_range(-1,1), 0).normalized()
 	GameController.emit_signal("ball_velocity_changed", direction*speed)
 
 
@@ -19,14 +25,18 @@ func _ready():
 func _physics_process(delta):
 	if GameController.is_active:
 		var collision = move_and_collide(direction * speed * delta)
-
 		if collision:
+			if !_has_bounced_once:
+				_has_bounced_once = true
+				speed *= 2  # Reset the speed after the first bounce
+
 			if collision.collider.get_collision_layer_bit(0): # World bit layer
 				direction = direction.bounce(collision.normal)
 				GameController.emit_signal("ball_velocity_changed", direction*speed)
 			elif collision.collider.get_collision_layer_bit(1): # Paddle bit layer
 				var diff = position - collision.collider.position
 				direction = diff.normalized()
+				direction.y = clamp(direction.y, -vertical_clamp, vertical_clamp)  # Prevents vertical bouncing
 				GameController.emit_signal("ball_velocity_changed", direction*speed)
 
 			if collision.collider.has_method("_on_ball_bounced"):
